b3d7af5640188d2989422629f63b157c59dbe0e4
Jake Taylor <me@jaketaylor.co>
2019-05-30 16:55:50 +0100

    Initial Commit

---
+++ Caddy.js
@@ -0,0 +1,203 @@
+window.CADDY_INSTANCE_COUNT = window.CADDY_INSTANCE_COUNT || 0;
+
+export class Caddy {
+    /**
+     * Constructs the class.
+     *
+     * @param {object} [options]
+     */
+    constructor (options) {
+        // Build up the options
+        this.options = this.buildOptions(options);
+
+        // Determine the key
+        const { key } = this.options;
+        this.key = key ? key : `caddy${++window.CADDY_INSTANCE_COUNT}`;
+
+        // Boot the store
+        const { driver } = this.options;
+        const store = driver.getItem(this.key);
+        this.store = store ? JSON.parse(store) : {};
+
+        // Initiate our subscribers as an empty array
+        this.subscribers = [];
+
+        // Initiate our listeners as an empty object
+        this.listeners = {};
+    }
+
+    /**
+     * Builds the options object.
+     *
+     * @param {object} options - The user provided options.
+     *
+     * @returns {object}
+     */
+    buildOptions (options) {
+        return {
+            key: null,
+            driver: window.localStorage,
+            ...options,
+        };
+    }
+
+    /**
+     * Takes the current store object and persists it using the configured driver.
+     */
+    persistStore () {
+        const { driver } = this.options;
+
+        this.prevStore = JSON.parse(driver.getItem(this.key));
+        driver.setItem(this.key, JSON.stringify(this.store));
+
+        this.notifySubscribers();
+        this.notifyListeners();
+    }
+
+    /**
+     * Calls every subscriber.
+     */
+    notifySubscribers () {
+        this.subscribers.forEach((subscriber) => {
+            subscriber(this.store);
+        });
+    }
+
+    /**
+     * Calls every appropriate listener.
+     */
+    notifyListeners () {
+        // Get the keys of both the previous and current stores.
+        const prevKeys = Object.keys(this.prevStore);
+        const currentKeys = Object.keys(this.store);
+
+        // Go through every current key
+        currentKeys.forEach(key => {
+            // Determine if the key is new or different
+            const isNew = !prevKeys.includes(key);
+            const isDifferent = this.prevStore[key] !== this.store[key];
+
+            // If so, loop through every associated listener and call
+            // it with the latest value.
+            if (isNew || isDifferent) {
+                const listeners = this.listeners[key] || [];
+
+                listeners.forEach((listener) => {
+                    listener(this.store[key]);
+                });
+            }
+        });
+    }
+
+    /**
+     * Sets an item in the store.
+     *
+     * @param {string} key - The key to store the value against.
+     * @param {*} val - The value to store.
+     *
+     * @returns {Caddy}
+     */
+    set (key, val) {
+        this.store[key] = val;
+        this.persistStore();
+
+        return this;
+    }
+
+    /**
+     * Gets an item from the store.
+     *
+     * @param {string} key - The key to retrieve.
+     *
+     * @returns {*}
+     */
+    get (key) {
+        return this.store[key];
+    }
+
+    /**
+     * Determines if the store has a given key.
+     *
+     * @param {string} key - The key to check.
+     *
+     * @returns {boolean}
+     */
+    has (key) {
+        return Object.prototype.hasOwnProperty.call(this.store, key);
+    }
+
+    /**
+     * Empties the entire store.
+     *
+     * @returns {Caddy}
+     */
+    flush () {
+        this.store = {};
+        this.persistStore();
+
+        return this;
+    }
+
+    /**
+     * Pushes an item into an array stored at the given key.
+     *
+     * @param {string} key - The key where the array is stored.
+     * @param {*} val - The value to push into the array.
+     *
+     * @returns {Caddy}
+     *
+     * @throws Will throw an error if the value at the given key is not an array.
+     */
+    push (key, val) {
+        // If there is nothing at the given key, we can just create a new array
+        if (!this.has(key)) {
+            return this.set(key, [val]);
+        }
+
+        // If the existing value isn't an array, we need to throw an error
+        if (!Array.isArray(this.get(key))) {
+            throw new Error('Failed trying to push to non-array.');
+        }
+
+        // Push the item into the array and save
+        this.store[key].push(val);
+        this.persistStore();
+
+        return this;
+    }
+
+    /**
+     * Registers a callback that will be called with the entire store
+     * whenever any sort of change is made.
+     *
+     * @param {function} subscriber - The callback to register as a subscriber.
+     *
+     * @returns {Caddy}
+     */
+    subscribe (subscriber) {
+        this.subscribers.push(subscriber);
+
+        return this;
+    }
+
+    /**
+     * Registers a listener for a given key that will be called whenever
+     * the value *changes*. If the new value is === the old value, the
+     * listener will not be called.
+     *
+     * @param {string} key - The key to listen to.
+     * @param {function} listener - The listener function to call.
+     *
+     * @returns {Caddy}
+     */
+    listen (key, listener) {
+        if (!this.listeners[key]) {
+            this.listeners[key] = [];
+        }
+
+        this.listeners[key].push(listener);
+
+        return this;
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,162 @@
+# Caddy
+
+A simple to use wrapper around JavaScript's LocalStorage/SessionStorage.
+
+Install from npm:
+
+```Bash
+npm install --save caddyjs
+
+# or yarn
+
+yarn add caddyjs
+```
+
+---
+
+## Usage
+
+Get started by creating an instance of the Caddy class:
+
+```JavaScript
+import { Caddy } from 'caddyjs';
+
+const caddy = new Caddy();
+```
+> The constructor takes an optional configuration object, see [here](#constructor).
+
+---
+
+## Documentation
+
+### Constructor
+
+The constructor takes an optional configuration object.
+
+```JavaScript
+const caddy = new Caddy({
+    key: 'my_custom_key',
+});
+```
+
+The options allowed are:
+
+| Key | Description | Default |
+| - | - | - |
+| `key` | The unique key that is used when storing items. | `caddy`_`X`_ where X is a number from 1 upwards*. |
+| `driver` | The storage driver to use. Must have the same interface as sessionStorage/localStorage. | `window.localStorage` |
+
+In most cases, the default options will be fine and you can just initiate caddy without any options:
+
+```JavaScript
+const caddy = new Caddy();
+```
+
+\* Caddy keeps track of how many instances have been created, and increments the key every time to avoid collisions.
+
+### set
+
+Adds an item into the storage.
+
+```JavaScript
+caddy.set('my_key', 'some sort of value');
+```
+
+`set` returns `this`, so you can chain multiple calls:
+
+```JavaScript
+caddy.set('key_a', 1)
+     .set('key_b', 2)
+```
+
+### get
+
+Gets an item from storage. Will return `undefined` if there is nothing stored against the given key.
+
+```JavaScript
+caddy.get('my_key') // 'some sort of value'
+caddy.get('key_we_havent_set') // undefined
+```
+
+### has
+
+Determines if a key exists in the store.
+
+```JavaScript
+caddy.has('key_a') // true
+caddy.has('key_b') // true
+caddy.has('key_c') // false
+```
+
+### push
+
+A convenience method that pushes the given value into an array stored at the given key. If there is nothing stored for the key, an array will be created with the value. If the value stored at the key is not an array, an error will be thrown.
+
+```JavaScript
+caddy.set('my_array', [1, 2, 3]);
+caddy.push('my_array', 4);
+caddy.get('my_array') // [1, 2, 3, 4]
+```
+
+```JavaScript
+caddy.has('new_key'); // false
+caddy.push('new_key', 3);
+caddy.get('new_key'); // [3]
+```
+
+```JavaScript
+caddy.set('key', 'string');
+caddy.push('key', 2); // Error!
+```
+
+`push` also returns `this`, so it can be chained if you wish:
+
+```JavaScript
+caddy.push('array', 1).push('array', 2);
+```
+
+### flush
+
+Empties the entire store.
+
+```JavaScript
+caddy.set('example', 123);
+caddy.has('example'); // true
+caddy.flush();
+caddy.has('example'); // false
+```
+
+### subscribe
+
+`subscribe` allows you to perform some logic whenever the store is updated. You supply a callback function that will be passed the entire store object.
+
+```JavaScript
+// Do something with the entire store
+caddy.subscribe((store) => {
+    console.log(store);
+});
+
+// The store is just an object, so you can destructure to get just the properties you're interested in:
+caddy.subscribe(({ my_key }) => {
+    console.log(my_key);
+});
+```
+
+You can subscribe to a store as many times as you wish.
+
+**See also:** [`listen`](#listen)
+
+
+### listen
+
+`listen` allows you to listen to **changes** to an item. It's similar to subscribe, but only for a specific key, and is only called when the value is different to what it was before. **Note:** the way Caddy compares values is simply the strict equality operator (`===`). Because of the way Caddy works, `listen` will not work properly if your value is an array or object, as Caddy will always see the value as different than last time. An alternative is to use [`subscribe`](#subscribe) and determine whether the value has changed yourself.
+
+```JavaScript
+caddy.listen('my_key', (value) => {
+    console.log(value);
+});
+```
+
+You can listen to a value as many times as you wish.
+
+**See also:** [`subscribe`](#subscribe)
